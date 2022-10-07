3be1f64d0b019b16c5725445c11631a940c28a4e
ruruisu <zaddozgl@gmail.com>
2022-10-07 10:01:38 -0500

    removed ability to register

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,15 @@ Rails.application.routes.draw do
   resources :costumers
   resources :products
   resources :employees
-  devise_for :users
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+  devise_for :users, :skip => [:registrations] 
+  as :user do
+  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
 
 end
